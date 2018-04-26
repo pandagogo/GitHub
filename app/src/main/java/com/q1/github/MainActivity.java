91ebcebd5244b8cefc1657fc87e308ec9f13fc4d
Q1 <skw1204@naver.com>
2018-04-26 14:30:26 +0900

    Initial commit

---
+++ app/src/main/java/com/q1/github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.q1.github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
